ca41cf2f66806c18159efdfb0aa9147a32e3fee3
coolzc <coolzc@gmail.com>
2019-01-30 16:54:51 +0800

    add count number

--- jisuanlilun_pachong.py
+++ jisuanlilun_pachong.py
@@ -6,24 +6,23 @@ from bs4 import BeautifulSoup
 if __name__ == '__main__':
 
     def download(url, file_name):
-        # open in binary mode
         with open(file_name, "wb") as file:
-            # get request
             response = requests.get(url)
-            # write to file
             file.write(response.content)
 
-    target = 'https://github.com/QSCTech/zju-icicles/tree/master/%E8%AE%A1%E7%AE%97%E7%90%86%E8%AE%BA/%E8%AF%95%E5%8D%B7'
+    target = ''
     req = requests.get(url=target)
     html = req.text
     bf = BeautifulSoup(html)
     texts = bf.find_all('a', class_='js-navigation-open')
     urls = []
+    count = 0
     for link in texts:
         download_url = link.get('href')
         if download_url != '':
             download_url = 'https://github.com' + download_url.replace('blob', 'raw')
             file_name = urllib.parse.unquote(download_url.split("/")[-1])
             download(download_url, file_name)
-            print('one file done')
+            count += 1
+            print(count + 'files done')
             urls.append(download_url)
